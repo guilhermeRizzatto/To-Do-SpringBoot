e133281fc028891f1e97430235c71c24ed0da8ac
Guilherme Rizzatto <guimarquesrz@gmail.com>
2024-08-05 16:05:56 -0300

    Created all the User classes for Api

---
+++ src/main/java/com/rizzatto/ToDo/services/UserService.java
@@ -0,0 +1,56 @@
+package com.rizzatto.ToDo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rizzatto.ToDo.dto.UserDtoRequest;
+import com.rizzatto.ToDo.entity.User;
+import com.rizzatto.ToDo.repository.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public User save(UserDtoRequest userRequest){
+		if(repository.findByEmail(userRequest.getEmail()).isEmpty()){
+			User user = UserDtoRequest.createUser(userRequest);
+			return repository.save(user);
+		}
+		throw new RuntimeException("This email already exist");
+	}
+	
+	public List<User> getUsers(){
+		return repository.findAllWithTasks();
+	}
+	
+	public Optional<User> getById(Long id) {
+		return repository.findById(id);
+	}
+	
+	public Optional<User> getByEmail(String email){
+		return repository.findByEmail(email);
+	}
+	
+	public User update(Long id, UserDtoRequest userToUpdate) {
+		User user = repository.getReferenceById(id);
+		
+		if(!user.getName().equals(userToUpdate.getName())) {
+			user.setName(userToUpdate.getName());
+		} else if(!user.getPassword().equals(userToUpdate.getPassword())) {
+			user.setPassword(userToUpdate.getPassword());
+		}
+		
+		return repository.save(user);
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
+	
+
+}
