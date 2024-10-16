dc0f39e51d641dbfc247eb667a94a9ce530838df
Guilherme <guimarquesrz@gmail.com>
2024-10-15 22:58:02 -0300

    added more configs in corsConfiguration.

--- src/main/java/com/rizzatto/ToDo/configuration/CorsConfiguration.java
+++ src/main/java/com/rizzatto/ToDo/configuration/CorsConfiguration.java
@@ -10,7 +10,9 @@ public class CorsConfiguration implements WebMvcConfigurer{
 	@Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("*");
+            .allowedOrigins("*")
+            .allowedMethods("*")
+            .allowedHeaders("*");
     }
 
 }
