1f8852aab47e16ec204b6abddc92ed3049e1fe85
Guilherme <guimarquesrz@gmail.com>
2024-11-19 21:26:31 -0300

    add localhost in cors

--- src/main/java/com/rizzatto/ToDo/configuration/CorsConfiguration.java
+++ src/main/java/com/rizzatto/ToDo/configuration/CorsConfiguration.java
@@ -10,7 +10,7 @@ public class CorsConfiguration implements WebMvcConfigurer {
 	    @Override
 	    public void addCorsMappings(CorsRegistry registry) {
 	        registry.addMapping("/**")
-	                .allowedOrigins("https://to-do-angular-delta.vercel.app")
+	                .allowedOrigins("https://to-do-angular-delta.vercel.app", "http://localhost:4200")
 	                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 	                .allowedHeaders("*")
 	                .allowCredentials(true);
